2cb61cb4ba53d19c3a7251cb3c85ac6cf723ee21
GFan99 <GFan99@users.noreply.github.com>
2018-10-25 14:27:30 +0200

    Klassenbeschreibungen Teil 1
    Beschreibung für Input und angefangene Beschreibung für Klassifikator

--- src/pack/Input.java
+++ src/pack/Input.java
@@ -1,5 +1,19 @@
 package pack;
 
+/**
+ * Dies wird die Klasse Input.
+ * Input dient dazu die bereitgestellte XML-Datei zu lesen und daraus die zu bearbeitenden Texte
+ * mit den gewünschten Labeln abzuleiten.
+ * Sie hat Zugriff auf die Klasse Klassifikator, damit sie dort in das label[] - Array die Namen
+ * der Label und in das text[] - Array die zu bearbeitenden Texte eintragen kann.
+ * Inout ist die erste Klasse, die von Steuerung angesprochen wird, da sie für das Laden der Da-
+ * ten zuständig ist, mit denen das Programm dann arbeiten soll.
+ * Zusätzlich kann sie die Dateien lesen, die erstellt werden, falls das Programm vor abarbeiten
+ * aller Texte beendet wird. Dies dient dazu, dass der Nutzer mit dem Klassifizieren/Labeln dort
+ * weitermachen kann, wo er zuvor aufgehört hat, ohne einen Text doppelt zu bearbeiten.
+ * @author susannabeck
+ * @version 0.1
+ */
 public class Input {
 
 }
--- src/pack/Klassifikator.java
+++ src/pack/Klassifikator.java
@@ -1,5 +1,14 @@
 package pack;
 
+/**
+ * Dies wird die Klasse Klassifikator.
+ * Sie ist der Mittelpunkt des gesamten Programms, denn in ihr liegen die Variablen, Methoden und
+ * Funktionen, die von allen anderen Klassen benutzt werden. So gibt es hier:
+ *  - labels[] : Das Array mit den Labels, nach denen die Texte "sortiert" werden.
+ *  - 
+ * @author becksusanna
+ * @version 0.1
+ */
 public class Klassifikator {
 
 }
