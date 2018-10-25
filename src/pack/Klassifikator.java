518330cfe4e11e04f35ebb02f0a0c1d15f6aa8b0
GFan99 <GFan99@users.noreply.github.com>
2018-10-25 14:55:57 +0200

    Umlautkorrekturen

--- src/pack/Klassifikator.java
+++ src/pack/Klassifikator.java
@@ -8,14 +8,14 @@ package pack;
  *  - texte[]    : Das Array mit den zu klassifizierenden Texten.
  *  - textids	 : Eine ArrayList, die die IDs der Texte speichert (ID=1 heißt (Index in texte[])=1).
  *  - ergebnisse : ergebnisse ist eine Map, die als Key die jeweilige textid (Int) und als Value ein 
- *  				boolean[] mit den für die Labels ausgewaehlten Werte hat.
+ *  				boolean[] mit den fuer die Labels ausgewaehlten Werte hat.
  *  - waehleText : Eine Funktion die aus textids zufaellig ein Element auswaehlt, dieses aus textids
- *  				entfernt und dann zurück gibt. Ist textids leer, so wird ein "Fehler-Wert" ueber-
- *  				geben (bspw. -100).
- *  - schrMap    : Eine Methode, die eine TextID (Int) und ein boolean[] übergeben bekommt und diese
- *  				dann als Schlüssel-Wert-Paar in ergebnisse eintraegt.
- *  - sortieren	 : Eine Funktion, die in der Lage ist, die übergebene Map aufsteigend nach dem Key zu
- *  				sortieren und dann die sortierte Map zurück gibt.
+ *  				entfernt und dann zurueck gibt. Ist textids leer, so wird ein "Fehler-Wert"
+ *  				uebergeben (bspw. -100).
+ *  - schrMap    : Eine Methode, die eine TextID (Int) und ein boolean[] uebergeben bekommt und diese
+ *  				dann als Schluessel-Wert-Paar in ergebnisse eintraegt.
+ *  - sortieren	 : Eine Funktion, die in der Lage ist, die uebergebene Map aufsteigend nach dem Key 
+ *  				zu sortieren und dann die sortierte Map zurueck gibt.
  * @author becksusanna
  * @version 0.1
  */
