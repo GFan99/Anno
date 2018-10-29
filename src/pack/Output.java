699c6aa31acc1d32f77b86d69c62dd9ad8625cf5
paddelbaer <40768309+paddelbaer@users.noreply.github.com>
2018-10-29 11:28:41 +0100

    Output bearbeitet

--- src/pack/Output.java
+++ src/pack/Output.java
@@ -1,5 +1,12 @@
 package pack;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.xml.parsers.*;
+
+import org.w3c.dom.*;
+
 /**
  * Dies wird die Klasse Output.
  * Sie ist zustaendig fuer alle Speichervorgaenge des Programms.
@@ -11,5 +18,39 @@ package pack;
  * @version 0.1
  */
 public class Output {
+	
+	public void speichern(HashMap<Integer,boolean[]> erg, String[] texte) {
+		
+		try {
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.newDocument();
+			
+			//ausgabe ist Wurzelelement
+			Element ausgabe = doc.createElement("ausgabe");
+			 for (int i=0; i<erg.size();i++) {
+				 Element text = doc.createElement("text"+i);
+				 text.appendChild(ausgabe);
+				 Element inhalt = doc.createElement("inhalt");
+				 inhalt.appendChild(text);
+				 Text inhalt2 = doc.createTextNode(texte[i]);
+				 inhalt2.appendChild(inhalt);
+				 
+				 
+			 }
+			
+			
+			
+			
+			
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void abbruchSave(ArrayList<Integer> id, String[] texte, String[] label, Map<Integer,boolean[]> erg) {
+		
+	}
 
 }
