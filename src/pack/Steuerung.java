91767605f12082575411952ee37fdb97a1768aa5
GFan99 <GFan99@users.noreply.github.com>
2018-10-29 11:44:29 +0100

    ToDo Fragen geklärt

--- src/pack/Steuerung.java
+++ src/pack/Steuerung.java
@@ -7,20 +7,19 @@ public class Steuerung {
 	public static void main(String[] args) {
 		/**
 		 *  ToDo: 
-		 *   - frage Nutzer nach seiner ID (hinweis geben, wie zu verfahren ist bei keiner id (leer 
-		 *     lassen ?) oder extra button?
+		 *   - frage Nutzer nach seiner ID (extra button fuer keine id)
 		 *   - prüfe, ob Speicherdatei mit nutzerid bereits vorhanden --> wenn ja laden
-		 *   - lade sonst xml-eingabe und generiere neue nutzerid --> wie? mit hashfkt um langen Zahlen- 
-		 *     und Buchstaben enthaltenden Code zu enthalten? zufallszahl + (statt *) nr des nutzers zum
-		 *     hashen nehmen? oder komplett zufällig bilden? hashfkt direkt auf nr des nutzers anwenden?
+		 *   - lade sonst xml-eingabe und generiere neue nutzerid --> wie? anz nutzerdat zaehlen, dann 
+		 *     zufallszahl zw 0 und 1 addieren, dann hash anwenden
 		 *   - variablen zuordnen
-		 *   - fenster starten (statisch: nutzerid-anzeige, schließ-knopf (?), textfeld (mit scrolleiste!?),
-		 *     ok-button, fortschrittsbalken bzw prozentangabe; dynamisch: auswahl"knöpfe" und labelname-
-		 *     feld) --> textgröße manuell einstellbar???
+		 *   - fenster starten 
+		 *     (statisch: nutzerid-anzeige, textfeld (mit scrolleiste!?), textgroesse, ok-button, 
+		 *     fortschrittsbalken bzw prozentangabe; 
+		 *     dynamisch: auswahl"knöpfe" und labelname-feld)
 		 *   - timer starten (eine Stunde) --> auch im fenster anzeigen
 		 *   - text auswählen und angaben in feld eintragen
 		 *   - wdh bis texte alle oder fenster geschlossen
-		 *   - texte alle --> anzeige, dass fertig; dann ausgabe-xml schreiben (mit nutzerid als titel?)
+		 *   - texte alle --> anzeige, dass fertig; dann ausgabe-xml schreiben (mit nutzerid als titel)
 		 *   - nutzer schließt fenster --> kleines fenster mit fortschritt; dann nutzerid.datei speichern
 		 *   - programm beenden
 		 */
